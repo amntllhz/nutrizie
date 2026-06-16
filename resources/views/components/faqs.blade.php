3ed12f84a71b23a3f06bddbff6bc5805bb1f1f36
amntllhz <amanatullah1904@gmail.com>
2026-06-16 23:46:46 +0700

    style: update faqs sections

--- resources/views/components/faqs.blade.php
+++ resources/views/components/faqs.blade.php
@@ -1,11 +1,11 @@
-<section class="max-w-6xl mx-auto p-10 sm:w-full sm:p-4">
+<section class="max-w-6xl mx-auto p-10 sm:w-full sm:p-4 mt-0 sm:mt-4">
     <div class="p-10 sm:p-2">
 
         <div class="space-y-2">
-            <h1 class="text-3xl font-bold text-center text-prim sm:text-2xl">Frequently <span
-                    class="text-acc">Asked</span> Questions</h1>
+            <h1 class="text-3xl font-bold text-center text-prim sm:text-xl">Frequently <span class="text-acc">Asked</span>
+                Questions</h1>
             <p class="text-gray-500 text-sm text-center sm:text-xs">Beberapa pertanyaan yang sering diajukan oleh
-                pengguna mengenai platform anaksehat</p>
+                pengguna mengenai platform Nutrizie</p>
         </div>
 
         <div class="mt-6">
@@ -17,7 +17,7 @@
                         class=" flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
                         data-accordion-target="#accordion-flush-body-1" aria-expanded="true"
                         aria-controls="accordion-flush-body-1">
-                        <span class="text-sm">Apa itu Anaksehat ?</span>
+                        <span class="text-sm sm:text-xs">Apa itu Nutrizie ?</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -27,7 +27,8 @@
                 </h2>
                 <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
                     <div class="py-5 border-b border-gray-200 dark:border-gray-700">
-                        <p class="mb-2 text-justify text-gray-500 text-sm dark:text-gray-400">anaksehat merupakan
+                        <p class="mb-2 text-justify text-gray-500 text-sm sm:text-xs dark:text-gray-400">Nutrizie
+                            merupakan
                             platform yang menyediakan informasi gizi balita, pola makan sehat, serta cara mengatasi
                             tantangan asupan untuk balita. Platform ini juga dilengkapi sistem Cek status gizi otomatis.
                         </p>
@@ -39,7 +40,7 @@
                         class=" flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
                         data-accordion-target="#accordion-flush-body-2" aria-expanded="false"
                         aria-controls="accordion-flush-body-2">
-                        <span class="text-sm text-left">Apa tujuan utama dari website Anaksehat ?</span>
+                        <span class="text-sm sm:text-xs text-left">Apa tujuan utama dari website Nutrizie ?</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -49,8 +50,9 @@
                 </h2>
                 <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
                     <div class="py-5 border-b border-gray-200 dark:border-gray-700">
-                        <p class="mb-2 text-justify text-gray-500 text-sm dark:text-gray-400">Tujuan utama website
-                            Anaksehat adalah memberikan informasi akurat dan terpercaya seputar gizi balita, serta
+                        <p class="mb-2 text-justify text-gray-500 text-sm sm:text-xs dark:text-gray-400">Tujuan utama
+                            website
+                            Nutrizie ialah untuk memberikan informasi akurat dan terpercaya seputar gizi balita, serta
                             memberikan edukasi mengenai makanan yang sehat dan seimbang untuk anak-anak di usia dini.
                             Kami juga berfokus pada penyuluhan tentang dampak kekurangan gizi dan cara-cara mencegahnya.
                         </p>
@@ -62,8 +64,9 @@
                         class=" flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3"
                         data-accordion-target="#accordion-flush-body-3" aria-expanded="false"
                         aria-controls="accordion-flush-body-3">
-                        <span class="text-sm text-left">Bagaimana cara mendapatkan informasi gizi yang tepat untuk
-                            balita di Anaksehat ?</span>
+                        <span class="text-sm sm:text-xs text-left">Bagaimana cara mendapatkan informasi gizi yang tepat
+                            untuk
+                            balita di Nutrizie ?</span>
                         <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -73,7 +76,8 @@
                 </h2>
                 <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
                     <div class="py-5 dark:border-gray-700">
-                        <p class="mb-2 text-justify text-gray-500 text-sm dark:text-gray-400">Anda dapat mengakses
+                        <p class="mb-2 text-justify text-gray-500 text-sm sm:text-xs dark:text-gray-400">Anda dapat
+                            mengakses
                             berbagai artikel, panduan, dan tips terkait gizi balita berdasarkan usia dan kebutuhan
                             spesifik. Kami menyediakan informasi yang mudah dipahami mengenai jenis makanan yang tepat,
                             jadwal makan yang ideal, serta tanda-tanda apabila anak mengalami kekurangan gizi. Anda juga
