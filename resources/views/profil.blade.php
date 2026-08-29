a9a4bb36eb40ff394795ccb95580c714fbbdafc7
amntllhz <amanatullah1904@gmail.com>
2026-08-29 16:22:32 +0700

    style: update profil & visi misi page

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -22,7 +22,7 @@
                 <div class="flex mx-auto items-center space-x-2.5 mb-8">
                     <p
                         class="font-semibold text-xs py-1.5 px-3.5 text-prim bg-prim/5 rounded-lg ring ring-inset ring-prim/10">
-                        Informasi Gizi</p>
+                        Artikel Gizi</p>
                     <p
                         class="font-semibold text-xs py-1.5 px-3.5 text-prim bg-prim/5 rounded-lg ring ring-inset ring-prim/10">
                         Cek Status Gizi</p>
@@ -33,7 +33,7 @@
 
                 <div class="space-y-3">
                     <p
-                        class="w-full text-gray-500 text-sm text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
+                        class="w-full text-gray-500 text-[13px] text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
                         <span class="font-bold text-prim">Nutrizie</span> adalah platform digital yang dirancang untuk
                         mendukung orang tua dan pengasuh dalam memastikan tumbuh kembang optimal balita melalui edukasi gizi
                         yang komprehensif. Kami hadir untuk memberikan informasi yang akurat, berbasis data ilmiah, dan
@@ -41,7 +41,7 @@
                         kesehatan si kecil.
                     </p>
                     <p
-                        class="w-full text-gray-500 text-sm text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
+                        class="w-full text-gray-500 text-[13px] text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
                         Melalui Nutrizie, Anda dapat menemukan berbagai artikel edukasi yang disusun oleh tim ahli gizi dan
                         kesehatan mengenai kebutuhan <span class="font-bold text-prim">Nutrisi Balita</span>, <span
                             class="font-bold text-prim">Menu Sehat</span>, hingga <span
@@ -50,7 +50,7 @@
                         oleh semua orang tua.
                     </p>
                     <p
-                        class="w-full text-gray-500 text-sm text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
+                        class="w-full text-gray-500 text-[13px] text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
                         Kami memahami bahwa <span class="font-bold text-prim">memantau gizi balita secara berkala</span>
                         adalah langkah penting dalam mendukung tumbuh kembangnya. Oleh karena itu, Nutrizie menyediakan
                         fitur <span class="font-bold text-prim">Cek Gizi Otomatis</span> yang memungkinkan Anda mengetahui
@@ -59,7 +59,7 @@
                         secara internasional.
                     </p>
                     <p
-                        class="w-full text-gray-500 text-sm text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
+                        class="w-full text-gray-500 text-[13px] text-justify leading-relaxed sm:w-full sm:text-xs sm:leading-relaxed">
                         Dengan semangat untuk mendukung generasi sehat Indonesia, Nutrizie terus berinovasi dalam
                         menghadirkan layanan yang relevan dan bermanfaat. Kami percaya bahwa investasi terbaik bagi masa
                         depan anak adalah melalui pemberian gizi yang optimal sejak dini. Mari bersama wujudkan anak-anak
